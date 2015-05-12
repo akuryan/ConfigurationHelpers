420389a036ddaf85021e531aedfb96477990e209
Anton Kuryan <a.kuryan@bylectric.com>
2015-05-12 11:51:46 +0300

    Added initial helpers

---
+++ IIS/UrlRewrite.ps1
@@ -0,0 +1,22 @@
+ function Is64Bit  
+ {  
+      [IntPtr]::Size -eq 8  
+ }  
+ function InstallIISRewriteModule(){  
+      $wc = New-Object System.Net.WebClient  
+      $dest = "IISRewrite.msi"  
+      $url  
+      if (Is64Bit){  
+           $url = "http://go.microsoft.com/?linkid=9722532"  
+      } else{  
+           $url = "http://go.microsoft.com/?linkid=9722533"       
+      }  
+      $wc.DownloadFile($url, $dest)  
+      msiexec.exe /i IISRewrite.msi /passive  
+ }  
+ if (!(Test-Path "$env:programfiles\Reference Assemblies\Microsoft\IIS\Microsoft.Web.Iis.Rewrite.dll")){  
+      InstallIISRewriteModule  
+ } else  
+ {  
+      Write-Host "IIS Rewrite Module - Already Installed..." -ForegroundColor Green  
+ }  
---
+++ IIS/configure.iis.on.server.cmd
@@ -0,0 +1,16 @@
+REM Enable IIS compression
+C:\Windows\System32\Inetsrv\Appcmd.exe unlock config /section:system.webserver/httpCompression /commit:apphost
+C:\Windows\System32\Inetsrv\Appcmd.exe set config -section:httpCompression -[name='gzip'].staticCompressionLevel:9 -[name='gzip'].dynamicCompressionLevel:4 /commit:apphost
+C:\Windows\System32\Inetsrv\Appcmd.exe set config -section:httpCompression -dynamicCompressionDisableCpuUsage:70 /commit:apphost
+C:\Windows\System32\Inetsrv\Appcmd.exe set config -section:system.webServer/httpCompression /+"staticTypes.[mimeType='application/msword',enabled='True']" /commit:apphost
+C:\Windows\System32\Inetsrv\Appcmd.exe set config -section:system.webServer/httpCompression /+"staticTypes.[mimeType='application/vnd.ms-powerpoint',enabled='True']" /commit:apphost
+C:\Windows\System32\Inetsrv\Appcmd.exe set config -section:system.webServer/httpCompression /+"staticTypes.[mimeType='application/vnd.ms-excel',enabled='True']" /commit:apphost
+C:\Windows\System32\Inetsrv\Appcmd.exe set config -section:system.webServer/httpCompression /+"dynamicTypes.[mimeType='text/*',enabled='True']" /commit:apphost
+C:\Windows\System32\Inetsrv\Appcmd.exe set config -section:system.webServer/httpCompression /+"dynamicTypes.[mimeType='message/*',enabled='True']" /commit:apphost
+C:\Windows\System32\Inetsrv\Appcmd.exe set config -section:system.webServer/httpCompression /+"dynamicTypes.[mimeType='application/x-javascript',enabled='True']" /commit:apphost
+C:\Windows\System32\Inetsrv\Appcmd.exe set config -section:system.webServer/httpCompression /+"dynamicTypes.[mimeType='application/atom+xml',enabled='True']" /commit:apphost
+C:\Windows\System32\Inetsrv\Appcmd.exe set config -section:system.webServer/httpCompression /+"dynamicTypes.[mimeType='application/xaml+xml',enabled='True']" /commit:apphost
+C:\Windows\System32\Inetsrv\Appcmd.exe set config -section:system.webServer/httpCompression /+"dynamicTypes.[mimeType='*/*',enabled='False']" /commit:apphost
+
+REM Install IIS UrlRewrite 2 module
+@powershell -NoProfile -ExecutionPolicy unrestricted .\UrlRewrite.ps1
