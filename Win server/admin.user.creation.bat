d7c745aabde6afe606f491f74e646ef96335953e
Anton Kuryan <a.kuryan@bylectric.com>
2015-06-11 14:15:16 +0300

    Added description

--- Win server/admin.user.creation.bat
+++ Win server/admin.user.creation.bat
@@ -1,3 +1,4 @@
 REM Invoke with username and password to create new user and add to administrators group
-NET USER %1 "%2" /ADD
+REM All switches can be read here - http://pcsupport.about.com/od/commandlinereference/p/net-user-command.htm
+NET USER %1 "%2" /fullname:"%3" /ADD
 NET LOCALGROUP "Administrators" "%1" /add
