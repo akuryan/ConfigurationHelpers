acb136b002e666698b206d0e2d787cc39a1dd9cc
Anton Kuryan <a.kuryan@colours.nl>
2016-03-18 12:53:30 +0300

    Webpicmd shall be installed by choco; Newrelic is still could not be installed by Choco

--- IIS/configure.server.cmd
+++ IIS/configure.server.cmd
@@ -62,7 +62,7 @@ C:\Windows\Microsoft.NET\Framework64\v2.0.50727\aspnet_regiis.exe -iru
 REM Install choco
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 REM Install notepad++
-choco install notepadplusplus webpi curl -y
+choco install notepadplusplus webpi webpicmd curl -y
 webpicmd /Install /Products:UrlRewrite2,WDeploy36PS /AcceptEULA
 REM You can search webpicmd in powershell for required ' webpicmd /List /ListOption:All | select-string -pattern "rewrite" '
 echo Install  Web Deploy 3.6 for Hosting servers and UrlRewrite2 from web platform installer yourself
@@ -86,4 +86,5 @@ net stop wuauserv
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v AUOptions /t REG_DWORD /d 3 /f
 net start wuauserv
 
-choco install newrelicserver newrelic-dotnet -y
+::Fails to install NewRelic
+REM choco install newrelicserver newrelic-dotnet -y
