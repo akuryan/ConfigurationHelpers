3fa71d5c0a14cdf4a7afc90d8a88659cc45191e0
Anton Kuryan <a.kuryan@colours.nl>
2016-07-07 20:50:50 +0200

    Added New relic server monitor installation script

---
+++ Azure/ArmTemplates/scripts/linux/install_newrelic_ubuntu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+licenseKey=$1
+
+echo deb http://apt.newrelic.com/debian/ newrelic non-free >> /etc/apt/sources.list.d/newrelic.list
+wget -O- https://download.newrelic.com/548C16BF.gpg | apt-key add -
+apt-get update
+apt-get install newrelic-sysmond
+nrsysmond-config --set license_key=$licenseKey
+/etc/init.d/newrelic-sysmond star
