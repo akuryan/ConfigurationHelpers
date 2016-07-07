a6d87bfded7f65e042ffab62ad17a8789f70636c
Anton Kuryan <a.kuryan@colours.nl>
2016-07-07 21:30:19 +0200

    Corrected installation instructions

--- Azure/ArmTemplates/scripts/linux/install_lap.sh
+++ Azure/ArmTemplates/scripts/linux/install_lap.sh
@@ -2,11 +2,8 @@
 
 # Install required packages
 echo "installing required packages"
-until apt-get -y update && apt-get -y install apache2 php5 php5-gd php5-mysql mysql-client git 
-do
-echo "installing required packages....."
-sleep 2
-done
+apt-get -y update
+apt-get -y install apache2 php5 php5-gd php5-mysql mysql-client git 
 
 # write some PHP
 echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/phpinfo.php
