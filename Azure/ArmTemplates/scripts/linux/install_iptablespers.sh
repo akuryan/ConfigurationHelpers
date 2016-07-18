8a3c5274abe1a45d24f446a67fbed1a37cd2559a
Anton Kuryan <a.kuryan@colours.nl>
2016-07-18 19:35:46 +0300

    Added iptables persistence installation

---
+++ Azure/ArmTemplates/scripts/linux/install_iptablespers.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
+
+apt-get update
+apt-get install -y iptables-persistent
