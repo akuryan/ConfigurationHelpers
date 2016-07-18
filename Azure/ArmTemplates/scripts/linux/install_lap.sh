e364aef37aabd1bd8095a5bd20ebb3f0f0c3d564
Anton Kuryan <a.kuryan@colours.nl>
2016-07-18 18:05:58 +0300

    Updated scripts

--- Azure/ArmTemplates/scripts/linux/install_lap.sh
+++ Azure/ArmTemplates/scripts/linux/install_lap.sh
@@ -9,5 +9,35 @@ apt-get -y install apache2 php5 php5-gd php5-mysql mysql-client git
 echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/phpinfo.php
 echo \<\?php phpinfo\(\)\; \?\> >> /var/www/html/phpinfo.php
 
+#Do some defaul Apache setup
+#Enable rewrite
+a2enmod rewrite
+#Set index.php as default doc
+if [ -f /etc/apache2/mods-enabled/dir.conf ]
+then
+	echo \<IfModule mod_dir.c\> > /etc/apache2/mods-enabled/dir.conf
+	echo DirectoryIndex index.php index.html >> /etc/apache2/mods-enabled/dir.conf
+	echo \</IfModule\> >> /etc/apache2/mods-enabled/dir.conf
+fi
+
+#Anable .htaccess and other not really secure stuff
+if [ -f /etc/apache2/sites-enabled/000-default.conf ]
+then
+	echo \<VirtualHost *:80\> > /etc/apache2/sites-enabled/000-default.conf
+		echo ServerAdmin a.kuryan@colours.nl >> /etc/apache2/sites-enabled/000-default.conf
+        echo DocumentRoot /var/www/html >> /etc/apache2/sites-enabled/000-default.conf
+        echo \<Directory /var/www/html/\> >> /etc/apache2/sites-enabled/000-default.conf
+                echo Options +Indexes +FollowSymLinks +MultiViews >> /etc/apache2/sites-enabled/000-default.conf
+                echo AllowOverride All >> /etc/apache2/sites-enabled/000-default.conf
+                echo Order allow,deny >> /etc/apache2/sites-enabled/000-default.conf
+                echo allow from all >> /etc/apache2/sites-enabled/000-default.conf
+        echo \</Directory\> >> /etc/apache2/sites-enabled/000-default.conf
+        echo RewriteEngine On >> /etc/apache2/sites-enabled/000-default.conf
+        echo RewriteOptions inherit >> /etc/apache2/sites-enabled/000-default.conf
+        echo ErrorLog ${APACHE_LOG_DIR}/error.log >> /etc/apache2/sites-enabled/000-default.conf
+        echo CustomLog ${APACHE_LOG_DIR}/access.log combined >> /etc/apache2/sites-enabled/000-default.conf
+	echo \</VirtualHost\> >> /etc/apache2/sites-enabled/000-default.conf
+fi
+
 # restart Apache
 apachectl restart
--- Azure/ArmTemplates/scripts/linux/install_mysql.sh
+++ Azure/ArmTemplates/scripts/linux/install_mysql.sh
@@ -5,3 +5,8 @@ dbpass=$1
 export DEBIAN_FRONTEND=noninteractive
 echo mysql-server-5.6 mysql-server/root_password password $dbpass | debconf-set-selections
 echo mysql-server-5.6 mysql-server/root_password_again password $dbpass | debconf-set-selections
+
+apt-get -y mysql-client mysql-server
+
+# Allow remote connection
+sed -i "s/bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
