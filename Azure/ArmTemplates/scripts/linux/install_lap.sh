3d5b4e6fca4e07b1b2531c97a4a64d5610c3383a
Anton Kuryan <a.kuryan@colours.nl>
2016-07-18 20:21:10 +0300

    Lap needs an unzip also

--- Azure/ArmTemplates/scripts/linux/install_lap.sh
+++ Azure/ArmTemplates/scripts/linux/install_lap.sh
@@ -3,7 +3,7 @@
 # Install required packages
 echo "installing required packages"
 apt-get -y update
-apt-get -y install apache2 php5 php5-gd php5-mysql mysql-client git 
+apt-get -y install apache2 php5 php5-gd php5-mysql mysql-client git unzip
 
 # write some PHP
 echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/phpinfo.php
