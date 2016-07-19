23a06f5ecdeabdbd1b4e7968350053318b475258
Anton Kuryan <a.kuryan@colours.nl>
2016-07-19 13:02:03 +0300

    Added apache restart

--- Azure/ArmTemplates/scripts/linux/install_newrelic_php_ubuntu.sh
+++ Azure/ArmTemplates/scripts/linux/install_newrelic_php_ubuntu.sh
@@ -6,3 +6,4 @@ appName=$2
 echo newrelic-php5 newrelic-php5/application-name string "$appName" | debconf-set-selections
 echo newrelic-php5 newrelic-php5/license-key string "$licenseKey" | debconf-set-selections
 apt-get install newrelic-php5 -y
+service apache2 restart
