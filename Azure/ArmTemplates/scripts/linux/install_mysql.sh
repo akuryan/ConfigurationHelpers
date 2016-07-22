3644b2a5fdaf102dc1c241470df4e68c39ee7e97
Anton Kuryan <a.kuryan@colours.nl>
2016-07-22 11:09:01 +0300

    Added keys adding feature, updated mysql installation scripts to install automysqlbackup

--- Azure/ArmTemplates/scripts/linux/install_mysql.sh
+++ Azure/ArmTemplates/scripts/linux/install_mysql.sh
@@ -7,7 +7,7 @@ echo mysql-server-5.6 mysql-server/root_password password $dbpass | debconf-set-
 echo mysql-server-5.6 mysql-server/root_password_again password $dbpass | debconf-set-selections
 
 apt-get update
-apt-get install -y mysql-client mysql-server
+apt-get install -y mysql-client mysql-server automysqlbackup
 
 # Allow remote connection
 sed -i "s/bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
---
+++ Azure/ArmTemplates/scripts/linux/keys.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo '#deployer Start' >> .ssh/authorized_keys
+curl https://github.com/deployer-colours-minsk.keys >> .ssh/authorized_keys
+echo '#deployer End' >> .ssh/authorized_keys
+echo '#mezinster Start' >> .ssh/authorized_keys
+curl https://github.com/mezinster.keys >> .ssh/authorized_keys
+echo '#mezinster End' >> .ssh/authorized_keys
+echo '#akuryan Start' >> .ssh/authorized_keys
+curl https://github.com/akuryan.keys >> .ssh/authorized_keys
+echo '#akuryan End' >> .ssh/authorized_keys
