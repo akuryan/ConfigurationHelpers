03be7bb7d1fe281442a684362d308b1c792a08b1
Anton Kuryan <a.kuryan@colours.nl>
2016-09-15 13:40:46 +0300

    Made chars for PW less to Mostly Alphabetic, Numeric, and a few “Specials”

--- IIS/GeneratePw.ps1
+++ IIS/GeneratePw.ps1
@@ -1,11 +1,11 @@
 # Generates password
-
+# Got it from https://blogs.technet.microsoft.com/heyscriptingguy/2013/06/03/generating-a-new-password-with-windows-powershell/
 Param(
 	[int]$length=15
 )
 
  
-$ascii=$NULL;For ($a=33;$a –le 126;$a++) {$ascii+=,[char][byte]$a }
+$ascii=$NULL;For ($a=48;$a –le 122;$a++) {$ascii+=,[char][byte]$a }
 For ($loop=1; $loop –le $length; $loop++) {
 
             $TempPassword+=($ascii | GET-RANDOM)
