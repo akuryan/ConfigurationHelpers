eab6a6e860b879b3b01212795c5eb11224bdfddf
Anton Kuryan <anton.kuryan@colours.nl>
2019-01-24 10:05:13 +0300

    Updated scwdp prep script

---
+++ Sitecore/MSDeploy/scwdp-prep-script/README.md
@@ -0,0 +1,3 @@
+# Usage
+
+Based on [script](https://github.com/robhabraken/Sitecore-Azure-Scripts/blob/master/Scripts/99%20Utility%20Scripts/strip-db.ps1), but do not requires passing separate parameters.xml file, as it could be extracted from scwdp by script
---
+++ Sitecore/MSDeploy/scwdp-prep-script/strip-db.ps1
@@ -0,0 +1,45 @@
+﻿[CmdletBinding()]
+Param(
+  [Parameter(Mandatory=$True)]
+  [string]$PackagePath,
+
+  [Parameter(Mandatory=$False)]
+  [string]$PackageDestinationPath = $($PackagePath).Replace(".scwdp.zip", "-nodb.scwdp.zip")
+)
+
+$msdeploy = "C:\Program Files\IIS\Microsoft Web Deploy V3\msdeploy.exe"
+$verb = "-verb:sync"
+$source = "-source:package=`"$PackagePath`""
+$destination = "-dest:package=`"$($PackageDestinationPath)`""
+$declareParamFile = "-declareparamfile=`"$($ParamFile)`""
+$skipDbFullSQL = "-skip:objectName=dbFullSql"
+$skipDbDacFx = "-skip:objectName=dbDacFx"
+$paramFileName = "parameters.xml"
+
+$ParamFile = $env:TEMP + "\" + $paramFileName
+Add-Type -Assembly System.IO.Compression.FileSystem
+$zip = [IO.Compression.ZipFile]::OpenRead($PackagePath)
+$zip.Entries | where {$_.Name -eq $paramFileName} | foreach {[System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, $ParamFile, $true)}
+$zip.Dispose()
+
+if(![System.IO.File]::Exists($ParamFile)){
+  # file with path $path doesn't exist
+  Write-Host "Could not find parameters file at $ParamFile"
+  exit 1
+}
+
+
+# read parameter file
+[xml]$paramfile_content = Get-Content -Path $ParamFile
+$paramfile_paramnames = $paramfile_content.parameters.parameter.name
+$params = ""
+foreach($paramname in $paramfile_paramnames){
+   $tmpvalue = "tmpvalue"
+   if($paramname -eq "License Xml"){ $tmpvalue = "LicenseContent"}
+   if($paramname -eq "IP Security Client IP"){ $tmpvalue = "0.0.0.0"}
+   if($paramname -eq "IP Security Client IP Mask"){ $tmpvalue = "0.0.0.0"}
+   $params = "$params -setParam:`"$paramname`"=`"$tmpvalue`""
+}
+
+# create new package
+Invoke-Expression "& '$msdeploy' --% $verb $source $destination $declareParamFile $skipDbFullSQL $skipDbDacFx $params"
