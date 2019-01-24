234cafbf00789eb3009a12bcd715b20a1f4c1d76
Anton Kuryan <anton.kuryan@colours.nl>
2019-01-24 10:35:27 +0300

    Loop over all scwdp packages in folder and create theirs no-database equivalent

--- Sitecore/MSDeploy/scwdp-prep-script/README.md
+++ Sitecore/MSDeploy/scwdp-prep-script/README.md
@@ -1,3 +1,5 @@
 # Usage
 
 Based on [script](https://github.com/robhabraken/Sitecore-Azure-Scripts/blob/master/Scripts/99%20Utility%20Scripts/strip-db.ps1), but do not requires passing separate parameters.xml file, as it could be extracted from scwdp by script
+
+It expects that all ```.scwdp.zip``` files would be grouped in one folder and it will loop over them to create no-databases equivalent packages.
--- Sitecore/MSDeploy/scwdp-prep-script/strip-db.ps1
+++ Sitecore/MSDeploy/scwdp-prep-script/strip-db.ps1
@@ -1,45 +1,49 @@
 ﻿[CmdletBinding()]
 Param(
   [Parameter(Mandatory=$True)]
-  [string]$PackagePath,
-
-  [Parameter(Mandatory=$False)]
-  [string]$PackageDestinationPath = $($PackagePath).Replace(".scwdp.zip", "-nodb.scwdp.zip")
+  [string]$PackagesFolder
 )
+
 
 $msdeploy = "C:\Program Files\IIS\Microsoft Web Deploy V3\msdeploy.exe"
 $verb = "-verb:sync"
-$source = "-source:package=`"$PackagePath`""
-$destination = "-dest:package=`"$($PackageDestinationPath)`""
-$declareParamFile = "-declareparamfile=`"$($ParamFile)`""
 $skipDbFullSQL = "-skip:objectName=dbFullSql"
 $skipDbDacFx = "-skip:objectName=dbDacFx"
 $paramFileName = "parameters.xml"
+$scwdpExtension = ".scwdp.zip"
 
-$ParamFile = $env:TEMP + "\" + $paramFileName
-Add-Type -Assembly System.IO.Compression.FileSystem
-$zip = [IO.Compression.ZipFile]::OpenRead($PackagePath)
-$zip.Entries | where {$_.Name -eq $paramFileName} | foreach {[System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, $ParamFile, $true)}
-$zip.Dispose()
-
-if(![System.IO.File]::Exists($ParamFile)){
-  # file with path $path doesn't exist
-  Write-Host "Could not find parameters file at $ParamFile"
-  exit 1
+$dirContent = Get-ChildItem $PackagesFolder
+foreach ($PackagePath in $dirContent | where {$_.FullName.EndsWith($scwdpExtension)} | % { $_.FullName }) {
+  $PackageDestinationPath = $PackagePath.Replace(".scwdp.zip", "-nodb.scwdp.zip")
+  $source = "-source:package=`"$PackagePath`""
+  $destination = "-dest:package=`"$($PackageDestinationPath)`""
+  
+  $ParamFile = $env:TEMP + "\" + $paramFileName
+  $declareParamFile = "-declareparamfile=`"$($ParamFile)`""
+  Add-Type -Assembly System.IO.Compression.FileSystem
+  $zip = [IO.Compression.ZipFile]::OpenRead($PackagePath)
+  $zip.Entries | where {$_.Name -eq $paramFileName} | foreach {[System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, $ParamFile, $true)}
+  $zip.Dispose()
+  
+  if(![System.IO.File]::Exists($ParamFile)){
+    # file with path $path doesn't exist
+    Write-Host "Could not find parameters file at $ParamFile"
+    continue
+  }
+  
+  
+  # read parameter file
+  [xml]$paramfile_content = Get-Content -Path $ParamFile
+  $paramfile_paramnames = $paramfile_content.parameters.parameter.name
+  $params = ""
+  foreach($paramname in $paramfile_paramnames){
+     $tmpvalue = "tmpvalue"
+     if($paramname -eq "License Xml"){ $tmpvalue = "LicenseContent"}
+     if($paramname -eq "IP Security Client IP"){ $tmpvalue = "0.0.0.0"}
+     if($paramname -eq "IP Security Client IP Mask"){ $tmpvalue = "0.0.0.0"}
+     $params = "$params -setParam:`"$paramname`"=`"$tmpvalue`""
+  }
+  
+  # create new package
+  Invoke-Expression "& '$msdeploy' --% $verb $source $destination $declareParamFile $skipDbFullSQL $skipDbDacFx $params"
 }
-
-
-# read parameter file
-[xml]$paramfile_content = Get-Content -Path $ParamFile
-$paramfile_paramnames = $paramfile_content.parameters.parameter.name
-$params = ""
-foreach($paramname in $paramfile_paramnames){
-   $tmpvalue = "tmpvalue"
-   if($paramname -eq "License Xml"){ $tmpvalue = "LicenseContent"}
-   if($paramname -eq "IP Security Client IP"){ $tmpvalue = "0.0.0.0"}
-   if($paramname -eq "IP Security Client IP Mask"){ $tmpvalue = "0.0.0.0"}
-   $params = "$params -setParam:`"$paramname`"=`"$tmpvalue`""
-}
-
-# create new package
-Invoke-Expression "& '$msdeploy' --% $verb $source $destination $declareParamFile $skipDbFullSQL $skipDbDacFx $params"
