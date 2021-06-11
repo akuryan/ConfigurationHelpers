8d0988305abe8455f945d54e5e39ab2817410ea0
Anton Kuryan <anton_kuryan@epam.com>
2021-06-11 09:26:04 +0300

    Installing git instead of curl; curl already part of windows

--- IIS/configure.server.cmd
+++ IIS/configure.server.cmd
@@ -60,7 +60,7 @@ C:\Windows\Microsoft.NET\Framework64\v4.0.30319\aspnet_regiis.exe -iru
 REM Install choco
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 REM Install notepad++
-choco install notepadplusplus webpi webpicmd curl -y --allow-empty-checksums
+choco install notepadplusplus webpi webpicmd git -y --allow-empty-checksums
 webpicmd /Install /Products:UrlRewrite2,WDeploy36PS /AcceptEULA
 REM You can search webpicmd in powershell for required ' webpicmd /List /ListOption:All | select-string -pattern "rewrite" '
 
